1ff54f9fc909fe12f08edbd1434eec0c3519be88
Rio Nur Arifin <59592379+arifinrio95@users.noreply.github.com>
2024-07-05 01:18:51 +0700

    Update ratespot.py

--- ratespot.py
+++ ratespot.py
@@ -506,10 +506,10 @@ def main():
         df['user_ratings_total'] = pd.to_numeric(df['user_ratings_total'], errors='coerce')
 
         # df['score'] = df['user_ratings_total']*df['rating']
-        df = df.sort_values(by=['rating', 'user_ratings_total'], ascending=[False, False])
+        
         df = df[df['rating'] > 4.2]
         df = df[df['user_ratings_total'] > 100]
-
+        df = df.sort_values(by=['rating', 'user_ratings_total'], ascending=[False, False])
         # # Lakukan min-max scaling pada kolom user_ratings_total dan rating
         # df['scaled_ratings'] = min_max_scale(df['user_ratings_total'])
         # df['scaled_rating'] = min_max_scale(df['rating'])
